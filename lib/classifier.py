69014f5c5f968c11bbc98231d75b819981865c70
Taleh Ibrahimli <me@talehibrahimli.com>
2018-11-23 18:02:00 +0400

    +fixes

---
+++ lib/classifier.py
@@ -0,0 +1,13 @@
+
+class ComplexClassifier:
+    def __init__(self):
+        pass
+
+    def addFeature(self):
+        pass
+
+
+
+class MavgComplexClassifier:
+    def __init__(self):
+        pass
